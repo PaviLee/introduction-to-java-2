d31a3063f5191a6a64316ccc1a2f4fbb2be549ed
PaviLee <28197286+PaviLee@users.noreply.github.com>
2020-07-17 00:07:50 -0700

    Create Class2Homework.java

---
+++ class2Homework_answerKey/Class2Homework.java
@@ -0,0 +1,25 @@
+package class2Homework_answerKey;
+
+import java.util.Scanner;
+
+public class Class2Homework {
+
+	public static void main(String[] args) {
+
+		// Practice with variables
+		int x = 300;
+		double y;
+		int a, b;
+		double c = 0.1, d = 0.2;
+
+		// Practice with user input
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter an integer: ");
+		int value1 = scanner.nextInt();
+		System.out.println("Please enter another integer: ");
+		int value2 = scanner.nextInt();
+
+		int sum = value1 + value2;
+		System.out.println("The sum of your integers is " + sum + ".");
+	}
+}
