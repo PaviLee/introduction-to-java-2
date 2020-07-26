033401a409ab0f6a938d5b348a86bd2b6f90a51f
PaviLee <28197286+PaviLee@users.noreply.github.com>
2020-07-26 16:54:28 -0700

    Update DivByNum.java

--- class4Homework_answerKey/DivByNum.java
+++ class4Homework_answerKey/DivByNum.java
@@ -11,6 +11,8 @@ public class DivByNum {
 		System.out.println("Please enter a divisor: ");
 		int divisor = scanner.nextInt();
 
+		scanner.close();
+		
 		System.out.println("List of all numbers divisible by " + divisor + " between 1 and 100: ");
 		for (int i = 1; i <= 100; i++) {
 			if (i % divisor == 0) {
