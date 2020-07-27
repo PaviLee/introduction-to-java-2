61e681ed91156e7802166b51629a93ea437ca20c
PaviLee <28197286+PaviLee@users.noreply.github.com>
2020-07-26 17:26:17 -0700

    Update Diff21.java

--- class3Homework_answerKey/Diff21.java
+++ class3Homework_answerKey/Diff21.java
@@ -9,18 +9,10 @@ public class Diff21 {
 	}
 
 	public static int diff21(int n) {
-		// if (n <= 21) {
-		// return 21 - n;
-		// } else {
-		// return (n - 21) * 2;
-		// }
-
-		int difference = 21 - n;
-
-		if (n > 21) {
-			return Math.abs(difference) * 2;
+		if (n <= 21) {
+			return 21 - n;
 		} else {
-			return Math.abs(difference);
+			return (n - 21) * 2;
 		}
 	}
 }
